9718cbd13913f977aeed38a4bee412613d5842ab
Guilherme Sales <gnailpo895@gmail.com>
2025-03-29 16:22:40 +0000

    Add database connection setup and print DataFrame in main.py; update requirements.txt for SQLAlchemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sqlalchemy import create_engine
 
 #For this to work you will need to pass your excel file to CSV format
 # and change the file path to the location of your file
@@ -7,7 +8,12 @@ import numpy as np
 #This will read the csv file 
 df = pd.read_csv('data.csv')
 
+print(df)
+
 #If the csv file is empty it will return an empty dataframe
 if(df.empty):
     print("DataFrame is empty")
+
+#This will make a conection to the database
+connection_string = ''
 
