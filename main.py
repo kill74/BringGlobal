f28413fc795c8d494bd1e90cde2d39436c99cea2
Guiii <gnailpo895@gmail.com>
2025-03-30 22:47:45 +0100

    Update main.py to include type hinting for SQLAlchemy import and add optional schema and database name specifications

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine # type: ignore
 
 #For this to work you will need to pass your excel file to CSV format
 # and change the file path to the location of your file
@@ -30,5 +30,12 @@ engine = create_engine(connection_string)
 #If necessary we can specify the table name
 #table_name = 'table_name'
 
+#If necessary we can specify the schema name
+#schema_name = 'schema_name'
+
+#If necessary we can specify the database name
+#database_name = 'database_name'
+
+#Continuar na segunda-feira
 
 
