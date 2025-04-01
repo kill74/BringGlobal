b82cf83d725c593c6d3b7bd5aad4454cfe411859
Guiii <gnailpo895@gmail.com>
2025-04-01 21:51:53 +0100

    Add XML file reading and SQL Server upload functionality in main.py; introduce .env file for configuration

--- main.py
+++ main.py
@@ -1,55 +1,46 @@
+# Import necessary libraries
 import pandas as pd
-import numpy as np
+import pyodbc
 import os
-from sqlalchemy import create_engine 
+from dotenv import load_dotenv  # New dependency
 
-#For this to work you will need to pass your excel file to CSV format
-# and change the file path to the location of your file
+# Load environment variables from .env file
+load_dotenv()
 
-#Make the program read Json Files too (TO DO)
-
-#This will read the csv file, if the file does not exist it will return an error message
-#If the file is empty it will return an empty dataframe
-file_path = 'data.csv'
-
-if not os.path.exists(file_path):
-    print (f"File {file_path} does not exist.")
-else:
-    df = pd.read_csv('data.csv')
-    df.dropna(inplace=True) # Remove rows with empty values
-
-
-#"to.string()" is converting the DataFrame to an string representation
-# and will print the entire DataFrame 
-# print(df.to_string())
-
-print(df)
-
-#This will only be used if you want to see the last 5 rows of the DataSet
-#print(df.tail()) 
-
-#This will only be used to see information about the DataSet
-#print(df.info())
-
-#If the csv file is empty it will return an empty dataframe
-if(df.empty):
-    print (f"File {file_path} is empty.")
+def xml_to_sql(xml_file_path, table_name):
+    """
+    Reads XML file and uploads it to SQL Server database
+    Parameters:
+    xml_file_path (str): Path to XML file
+    table_name (str): Target table name
+    """
     
-#This will make a conection to the database
-connection_string = 'database_conection'
+    # Get credentials from environment variables
+    server = os.getenv('SERVER')
+    database = os.getenv('DATABASE')
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
 
-#Create the engine
-engine = create_engine(connection_string)
+    # Read XML file using pandas
+    try:
+        df = pd.read_xml(xml_file_path)
+        print("XML file successfully read!")
+        print("Data preview:")
+        print(df.head())
+    except Exception as e:
+        print(f"Error reading XML file: {e}")
+        return
 
-#If necessary we can specify the table name
-#table_name = 'table_name'
-
-#If necessary we can specify the schema name
-#schema_name = 'schema_name'
-
-#If necessary we can specify the database name
-#database_name = 'database_name'
-
-
-
-
+    # Build connection string dynamically
+    connection_string = f"""
+        DRIVER={{SQL Server}};
+        SERVER={server};
+        DATABASE={database};
+    """
+    
+    # Add authentication based on available credentials
+    if username and password:
+        connection_string += f"UID={username};PWD={password};"
+    else:
+        connection_string += "Trusted_Connection=yes;"
+        print("Using Windows authentication")
