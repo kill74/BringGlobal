440e3acc7d0c6ce49aae8caf2e4b95fe78124d0d
Pedro Mouro <pmcmouro@gmail.com>
2025-04-21 15:43:43 +0100

    gg

--- ExcelToXML_DB_Converter/teste_excel.py
+++ ExcelToXML_DB_Converter/teste_excel.py
@@ -1,18 +1,22 @@
 import pandas as pd
 
 # Caminho para o Excel
-file_path = r"C:/Users/tiago/Documents/BringGlobal/ExcelToXML_DB_Converter/Excel/2024/Bring_Contribuicoes_BPI_2024.12.xlsx"
+file_path = r"C:\Users\pmcmo\Documents\BringGlobal-1\ExcelToXML_DB_Converter\Excel\2024\Bring_Contribuicoes_BPI_2024.12.xlsx"
 
 
-df_preview = pd.read_excel(file_path, header=None, nrows=100, engine="openpyxl")
+df_preview = pd.read_excel(file_path, sheet_name="Mapa contribuições" , header=None, nrows=100, engine="openpyxl")
 
-print("Primeiras 10 linhas do Excel:")
+
 print(df_preview)
+
 
 # lê a folha normalmente 
 try:
-    df = pd.read_excel(file_path, sheet_name="Mapa contribuições", dtype=str, engine="openpyxl")
-    print("\nColunas detectadas:")
+    df = pd.read_excel(file_path, sheet_name="Mapa contribuições" ,nrows=100, dtype=str, engine="openpyxl")
+  #  print("\nColunas detectadas:")
     print(df.columns.tolist())
+
 except Exception as e:
     print(f"Erro ao ler o ficheiro: {e}")
+
+
